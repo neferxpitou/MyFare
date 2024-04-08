ae940e95288e8018aac583ecae5ee12ad4644144
Youssef <youssef.abdelrhafour@ucalgary.ca>
2024-04-07 18:03:20 -0600

    Contact Us Page

---
+++ src/pages/ContactUs/ContactUs.jsx
@@ -0,0 +1,11 @@
+const ContactUs = () => {
+	return (
+		<div>
+			<h1>Contact Us</h1>
+			<p>For any queries, please contact us at:</p>
+			<p>Email:</p>
+		</div>
+	);
+};
+
+export default ContactUs;
