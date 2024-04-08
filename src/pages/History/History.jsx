7d6df5b5a705d9522b221c42c253933b4fe4c705
Youssef <youssef.abdelrhafour@ucalgary.ca>
2024-04-07 18:04:19 -0600

    History Page

--- src/pages/History/History.jsx
+++ src/pages/History/History.jsx
@@ -1,11 +1,27 @@
 import Navbar from '../../components/Navbar/Navbar';
 import './History.css';
+
 const History = () => {
 	return (
-		<div>
+		<div className="history-container">
 			<Navbar />
-			<h1>History</h1>
-			<p>History of tickets purchased</p>
+			<div className="history-content">
+				<div className="history-title">Transaction History</div>
+
+				<div className="list-of-transactions">
+					<button className="transaction-item">
+						<div className="transaction-section-one">
+							<div className="transaction-title">Adult Single</div>
+							<div className="transaction-date">02-16-2024</div>
+						</div>
+
+						<div className="transaction-section-two">
+							<div className="transaction-amount">$3.70</div>
+							<div className="Details">Tap for More Details</div>
+						</div>
+					</button>
+				</div>
+			</div>
 		</div>
 	);
 };
