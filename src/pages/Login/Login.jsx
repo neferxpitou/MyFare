0d1b0786210b4116613e11e085f3acf300f3800d
Youssef <youssef.abdelrhafour@ucalgary.ca>
2024-04-07 18:04:31 -0600

    Login Page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,9 +1,43 @@
+import React from 'react';
 import './Login.css';
+import DirectionsBusIcon from '@mui/icons-material/DirectionsBus';
+import { useNavigate } from 'react-router-dom';
+
 const Login = () => {
+	const navigate = useNavigate();
+
 	return (
-		<div>
-			<h1>Login</h1>
-			<button>Login</button>
+		<div className="login-container">
+			<div className="app-title">
+				<DirectionsBusIcon /> MyFare
+			</div>
+			<div className="login-input-fields">
+				<input
+					type="text"
+					placeholder="Email or Username"
+					className="login-email-input"
+				/>
+				<input
+					type="password"
+					placeholder="Password"
+					className="login-password-input"
+				/>
+			</div>
+			<div className="forgot-password">
+				<a href="/login">Forgot Password?</a>
+			</div>
+			<div className="login-wrapper">
+				<button className="login-button">Login</button>
+			</div>
+			<div className="divider">New to MyFare?</div>
+			<div className="create-wrapper">
+				<button
+					className="create-button"
+					onClick={() => navigate('/create-account')}
+				>
+					Create Account
+				</button>
+			</div>
 		</div>
 	);
 };
