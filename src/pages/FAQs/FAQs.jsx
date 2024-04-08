6fa80b8517681fb0b34ef39e3163d8e5e1c48942
Youssef <youssef.abdelrhafour@ucalgary.ca>
2024-04-07 18:03:53 -0600

    FAQs Page

---
+++ src/pages/FAQs/FAQs.jsx
@@ -0,0 +1,29 @@
+const FAQs = () => {
+	return (
+		<div>
+			<h1>Frequently Asked Questions</h1>
+			<h2>What is MyFare?</h2>
+			<p>
+				MyFare is a bus ticket booking application that allows you to book bus
+				tickets for various routes.
+			</p>
+			<h2>How do I book a ticket?</h2>
+			<p>
+				To book a ticket, you need to create an account and log in. Once you are
+				logged in, you can search for buses, select your preferred bus, and book
+				a ticket.
+			</p>
+			<h2>What payment methods are accepted?</h2>
+			<p>We accept payments via credit card, debit card, and net banking.</p>
+			<h2>Can I cancel my ticket?</h2>
+			<p>
+				Yes, you can cancel your ticket. However, cancellation charges may apply
+				depending on the time of cancellation.
+			</p>
+			<h2>How do I contact customer support?</h2>
+			<p>You can contact customer support by emailing us at</p>
+		</div>
+	);
+};
+
+export default FAQs;
