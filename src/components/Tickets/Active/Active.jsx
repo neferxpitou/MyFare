ee2c8f6dca2f1b54c9b34340802a2bb9ecf64db9
Youssef <youssef.abdelrhafour@ucalgary.ca>
2024-04-09 18:51:18 -0600

    Update Active Ticket Component

--- src/components/Tickets/Active/Active.jsx
+++ src/components/Tickets/Active/Active.jsx
@@ -1,17 +1,30 @@
 import React from 'react';
-import './Unactive.css';
+import { useNavigate } from 'react-router-dom';
+import './Active.css';
 import InfoIcon from '@mui/icons-material/Info';
 
-const Unactive = () => {
+const Active = ({ details }) => {
+	const navigate = useNavigate();
+
+	const handleTicketClick = () => {
+		navigate('/ticket-details', { state: { ticket: details } });
+	};
+
 	return (
-		<div className="unactive">
-			<button className="ticket-button">
-				ADULT DAY PASS
-				<InfoIcon style={{ marginLeft: '8px', verticalAlign: 'middle' }} />
+		<div className="active">
+			<button className="active-ticket-button" onClick={handleTicketClick}>
+				<div className="ticket-info">
+					{details.type}
+					<InfoIcon style={{ marginLeft: '8px' }} />
+				</div>
+				<img
+					src={`${process.env.PUBLIC_URL}/adobe-express-qr-code.png`}
+					alt="QR Code"
+					className="home-qr-code"
+				/>
 			</button>
-			<button className="activate-button">Tap to Activate</button>
 		</div>
 	);
 };
 
-export default Unactive;
+export default Active;
