cafc321ba39c81c785320a2434145eaa634bdf15
Youssef <youssef.abdelrhafour@ucalgary.ca>
2024-04-09 18:49:19 -0600

    Update Contact Us Page

--- src/pages/ContactUs/ContactUs.jsx
+++ src/pages/ContactUs/ContactUs.jsx
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import Navbar from '../../components/Navbar/Navbar';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 import './ContactUs.css';
 
 const ContactUs = () => {
+	const navigate = useNavigate();
+	const [email, setEmail] = useState('');
+	const [message, setMessage] = useState('');
+	const [showConfirmation, setShowConfirmation] = useState(false);
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		if (email && message) {
+			setShowConfirmation(true);
+			setTimeout(() => {
+				navigate('/home');
+			}, 2000);
+		}
+	};
+
 	return (
 		<div className="contactus-container">
 			<Navbar />
+
+			{showConfirmation && <div className="overlay"></div>}
+
 			<div className="contactus-content">
-				<div className="contactus-title">Customer Service</div>
+				<div className="contactus-title">Contact Us</div>
 				<div className="contactus-section">
 					<div className="contactus-heading">Phone</div>
 					<div className="contactus-details">(403) 268-1969</div>
@@ -21,17 +41,27 @@ const ContactUs = () => {
 						For any issues with the application, please fill out the form below
 						and we will get back to you as soon as possible.
 					</div>
-					<form className="contactus-form">
+					<form className="contactus-form" onSubmit={handleSubmit}>
 						<input
 							className="contactus-input"
 							type="text"
 							placeholder="Your Email Address"
+							value={email}
+							onChange={(e) => setEmail(e.target.value)}
 						/>
 						<textarea
 							className="contactus-input"
 							placeholder="Type in your Message"
+							value={message}
+							onChange={(e) => setMessage(e.target.value)}
 						></textarea>
-						<button className="send-email">Send Email</button>
+						<button
+							className={`send-email ${email && message ? '' : 'disabled'}`}
+							type="submit"
+							disabled={!email || !message}
+						>
+							Send Email
+						</button>
 					</form>
 				</div>
 				<hr className="contactus-divider" />
@@ -41,6 +71,12 @@ const ContactUs = () => {
 				</div>
 				<hr className="contactus-divider" />
 			</div>
+			{showConfirmation && (
+				<div className="confirmation-popup">
+					<CheckCircleIcon style={{ color: 'green', fontSize: '40px' }} />
+					<p>Email sent successfully!</p>
+				</div>
+			)}
 		</div>
 	);
 };
