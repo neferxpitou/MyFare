7047f17001d69e31e7b35c7bfb2f8ca33fc115f8
Youssef <youssef.abdelrhafour@ucalgary.ca>
2024-04-09 18:48:02 -0600

    Update Profile Page

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,13 +1,69 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import ProfileNav from '../../components/Navbar/ProfileNav';
 import './Profile.css';
+import CreditCardIcon from '@mui/icons-material/CreditCard';
+import EventRepeatIcon from '@mui/icons-material/EventRepeat';
+import ReceiptIcon from '@mui/icons-material/Receipt';
+import LogoutModal from '../../components/Buttons/LogoutModal/LogoutModal';
 
 const Profile = () => {
 	const navigate = useNavigate();
+	const [isLogoutModalOpen, setLogoutModalOpen] = useState(false);
+	const [newEmail, setNewEmail] = useState('');
+	const [newPassword, setNewPassword] = useState('');
+	const [retypePassword, setRetypePassword] = useState('');
+
+	const isEmailValid = (email) => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+
+	const [showSuccessPopup, setShowSuccessPopup] = useState(false);
+	const [successMessage, setSuccessMessage] = useState('');
+
+	const logout = () => {
+		localStorage.removeItem('loggedInUser');
+		navigate('/');
+	};
+
+	const saveNewEmail = () => {
+		const loggedInUser = localStorage.getItem('loggedInUser');
+		if (loggedInUser && isEmailValid(newEmail)) {
+			const userData = JSON.parse(localStorage.getItem(loggedInUser));
+			userData.email = newEmail;
+			localStorage.setItem(loggedInUser, JSON.stringify(userData));
+			localStorage.setItem(newEmail, JSON.stringify(userData));
+			localStorage.removeItem(loggedInUser);
+			localStorage.setItem('loggedInUser', newEmail);
+
+			setSuccessMessage('Email updated successfully.');
+			setShowSuccessPopup(true);
+
+			setTimeout(() => {
+				navigate('/home');
+			}, 2000);
+		}
+	};
+
+	const saveNewPassword = () => {
+		if (newPassword === retypePassword) {
+			const loggedInUser = localStorage.getItem('loggedInUser');
+			if (loggedInUser) {
+				const userData = JSON.parse(localStorage.getItem(loggedInUser));
+				userData.password = newPassword;
+				localStorage.setItem(loggedInUser, JSON.stringify(userData));
+
+				setSuccessMessage('Password updated successfully.');
+				setShowSuccessPopup(true);
+
+				setTimeout(() => {
+					navigate('/home');
+				}, 2000);
+			}
+		}
+	};
+
 	return (
 		<div className="profile-container">
-			<ProfileNav />
+			<ProfileNav openLogoutModal={() => setLogoutModalOpen(true)} />
 			<div className="profile-content">
 				<div className="update-email">
 					<div className="update-title">Update Email:</div>
@@ -15,8 +71,16 @@ const Profile = () => {
 						type="email"
 						placeholder="New Email Address"
 						className="profile-email-input"
+						value={newEmail}
+						onChange={(e) => setNewEmail(e.target.value)}
 					/>
-					<button className="save-email-button">Save Email</button>
+					<button
+						className="save-email-button"
+						onClick={saveNewEmail}
+						disabled={!isEmailValid(newEmail)}
+					>
+						Save Email
+					</button>
 				</div>
 				<div className="update-password">
 					<div className="update-title">Update Password:</div>
@@ -24,36 +88,62 @@ const Profile = () => {
 						type="password"
 						placeholder="New Password"
 						className="profile-password-input"
+						value={newPassword}
+						onChange={(e) => setNewPassword(e.target.value)}
 					/>
 					<input
 						type="password"
 						placeholder="Retype New Password"
 						className="profile-password-input"
+						value={retypePassword}
+						onChange={(e) => setRetypePassword(e.target.value)}
 					/>
-					<button className="save-password-button">Save Password</button>
+					<button
+						className="save-password-button"
+						onClick={saveNewPassword}
+						disabled={
+							!newPassword || !retypePassword || newPassword !== retypePassword
+						}
+					>
+						Save Password
+					</button>
 				</div>
 				<div className="custom-divider"></div>
 				<div className="separate">
 					<button
 						className="profile-button"
-						onClick={() => navigate('/contact-us')}
+						onClick={() => navigate('/manage-credit-cards')}
 					>
-						Credit Cards
+						Manage Credit Cards <CreditCardIcon />
 					</button>
 					<button
 						className="profile-button"
 						onClick={() => navigate('/manage-subscriptions')}
 					>
-						Subscriptions
+						Manage Subscriptions <EventRepeatIcon />
 					</button>
 					<button
 						className="profile-button"
 						onClick={() => navigate('/history')}
 					>
-						Transaction History
+						Transaction History <ReceiptIcon />
 					</button>
 				</div>
 			</div>
+			{showSuccessPopup && (
+				<>
+					<div className="popup-backdrop"></div>
+					<div className="success-popup">
+						<p>{successMessage}</p>
+					</div>
+				</>
+			)}
+
+			<LogoutModal
+				isOpen={isLogoutModalOpen}
+				onClose={() => setLogoutModalOpen(false)}
+				onLogout={logout}
+			/>
 		</div>
 	);
 };
