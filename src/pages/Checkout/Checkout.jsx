5ad71a035124d39d3aa6fb51874c218d15ba6c72
Youssef <youssef.abdelrhafour@ucalgary.ca>
2024-04-09 18:49:26 -0600

    Update Checkout Page

--- src/pages/Checkout/Checkout.jsx
+++ src/pages/Checkout/Checkout.jsx
@@ -4,15 +4,68 @@ import './Checkout.css';
 import ApplePay from '../../components/Buttons/ApplePay/ApplePay';
 import ClearIcon from '@mui/icons-material/Clear';
 import CreditCardDropdown from '../../components/Dropdown/CreditCardDropdown';
+import { useNavigate } from 'react-router-dom';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 
 const Checkout = () => {
+	const navigate = useNavigate();
 	const [ticketSelections, setTicketSelections] = useState([]);
+	const [selectedCard, setSelectedCard] = useState(null);
+	const [showPurchaseComplete, setShowPurchaseComplete] = useState(false);
 
 	useEffect(() => {
-		const selections =
-			JSON.parse(localStorage.getItem('ticketSelections')) || [];
-		setTicketSelections(selections);
+		const loggedInUser = localStorage.getItem('loggedInUser');
+		if (loggedInUser) {
+			const user = JSON.parse(localStorage.getItem(loggedInUser));
+			const selections = user.selectedTickets;
+			setTicketSelections(selections || []);
+		}
 	}, []);
+
+	const handlePurchase = () => {
+		const loggedInUser = localStorage.getItem('loggedInUser');
+		if (loggedInUser && ticketSelections.length > 0) {
+			const user = JSON.parse(localStorage.getItem(loggedInUser));
+
+			const newPurchasedTickets = ticketSelections.map((selectedTicket) => ({
+				...selectedTicket,
+				id: `TICKET-${Date.now()}-${Math.random().toString(36).substr(2, 9)}`,
+			}));
+
+			user.purchasedTickets = [
+				...user.purchasedTickets,
+				...newPurchasedTickets,
+			];
+			user.selectedTickets = [];
+
+			localStorage.setItem(loggedInUser, JSON.stringify(user));
+
+			setShowPurchaseComplete(true);
+
+			setTimeout(() => {
+				setShowPurchaseComplete(false);
+				navigate('/home');
+			}, 3000);
+		}
+	};
+
+	const handleRemoveTicket = (indexToRemove) => {
+		const updatedTicketSelections = ticketSelections.filter(
+			(_, index) => index !== indexToRemove
+		);
+		setTicketSelections(updatedTicketSelections);
+
+		const loggedInUser = localStorage.getItem('loggedInUser');
+		if (loggedInUser) {
+			const user = JSON.parse(localStorage.getItem(loggedInUser));
+			user.selectedTickets = updatedTicketSelections;
+			localStorage.setItem(loggedInUser, JSON.stringify(user));
+		}
+
+		if (updatedTicketSelections.length === 0) {
+			navigate('/tickets');
+		}
+	};
 
 	const totalPrice = ticketSelections.reduce(
 		(total, item) => total + item.quantity * item.price,
@@ -30,23 +83,41 @@ const Checkout = () => {
 							<div className="number-of-tickets">{item.quantity} x</div>
 							<div className="type-of-ticket">{item.type}</div>
 							<div className="price-of-ticket">${item.price.toFixed(2)}</div>
-							<ClearIcon className="clear-icon" />
+							<ClearIcon
+								className="clear-icon"
+								onClick={() => handleRemoveTicket(index)}
+							/>
 						</div>
 					))}
 				</div>
 				<div className="checkout-total">
-					<div className="checkout-total-title">Total: </div>
+					<div className="checkout-total-title">Total:</div>
 					<div className="checkout-total-price">${totalPrice.toFixed(2)}</div>
 				</div>
 				<div className="buy-apple-pay">
-					<ApplePay />
+					<ApplePay onClick={handlePurchase} />
 				</div>
 				<div className="divider-wrapper">
 					<div className="payment-divider">OR</div>
 				</div>
-				<CreditCardDropdown />
-				<button className="checkout-button">Checkout</button>
+				<CreditCardDropdown onSelect={setSelectedCard} />{' '}
+				<button
+					className={`checkout-button ${selectedCard ? '' : 'disabled'}`}
+					onClick={handlePurchase}
+					disabled={!selectedCard}
+				>
+					Checkout
+				</button>
 			</div>
+			{showPurchaseComplete && (
+				<>
+					<div className="overlay-two"></div>
+					<div className="purchase-complete-popup">
+						<CheckCircleIcon style={{ color: 'green', fontSize: '40px' }} />
+						<p>Purchase Complete</p>
+					</div>
+				</>
+			)}
 		</div>
 	);
 };
